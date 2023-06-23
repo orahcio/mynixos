c6b4124afcb247d12c7b5967c72c6560202bcca0
orahcio <orahcio@gmail.com>
2023-06-22 22:05:27 -0300

    Corrigido um bug adicionando versão no input do home-manager

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
