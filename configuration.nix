d4cb8f24f56cfb2cb076087ff67d246ed85a5283
orahcio <orahcio@gmail.com>
2024-03-17 16:44:51 -0300

    Retirei a nvidia e não deu certo o plasma6, só aparece o mouse

--- configuration.nix
+++ configuration.nix
@@ -3,15 +3,7 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
-let
-  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec -a "$0" "$@"
-  '';
-in
+
 {
   imports =
     [ # Include the results of the hardware scan. # home-manager.nixosModule
@@ -89,44 +81,7 @@ in
     driSupport32Bit = true;
   };
 
-  services.xserver.videoDrivers = [ "nvidia" ]; # "modesetting"
-
-  hardware.nvidia = {
-    
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = true;
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-	  # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-    prime = {
-      offload = {
-	      enable = true;
-	      enableOffloadCmd = true;
-	    };
-		  nvidiaBusId = "PCI:1:0:0";
-		  intelBusId = "PCI:0:2:0";
-    };
-  };
+  services.xserver.videoDrivers = [ "modesetting" ]; # "nvidia"
 
   hardware.opentabletdriver = {
     enable = true;
