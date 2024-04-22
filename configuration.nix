ed8782f67e7f0e480ae1f77a6c6f5a53d8317769
orahcio <orahcio@gmail.com>
2024-04-22 11:32:11 -0300

    Atualização e removendo o service xserver, indo direto pra displayManager

--- configuration.nix
+++ configuration.nix
@@ -135,9 +135,8 @@ in
 
   # Enable the KDE Plasma Desktop Environment.
   services.desktopManager.plasma6.enable = true;
-  services.xserver.displayManager = {
+  services.displayManager = {
     sddm.wayland.enable = true;
-    # lightdm.enable = true;
     defaultSession = "plasma";
     autoLogin.user = "orahcio";
   };
