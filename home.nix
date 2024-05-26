0d1d615754503be9d87ca3b32732bef500e0bc20
orahcio <orahcio@gmail.com>
2024-05-26 01:08:39 -0300

    Uns plugins para o neovim

--- home.nix
+++ home.nix
@@ -275,17 +275,62 @@
     userEmail = "orahcio@gmail.com";
   };
   
-  programs.neovim = {
+  programs.neovim =
+  let
+    toLua = str: "lua << EOF\n${str}\nEOF\n";
+    toLuaFile = file: "lua << EOF\n${builtins.readFile file}}\nEOF\n";
+  in  
+  {
     enable = true;
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
-    extraConfig = ''
-      set number relativenumber
-    '';  
-    extraLuaConfig = ''
-      ${builtins.readFile ./lazy.lua}
-    '';
+    # extraConfig = ''
+      # set number relativenumber
+    # '';  
+    # extraLuaConfig = ''
+      # ${builtins.readFile ./lazy.lua}
+    # '';
+    plugins = with pkgs.vimPlugins; [
+      {
+        plugin = telescope-nvim;
+        config = toLua ''
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})'';
+      }
+      nvim-treesitter
+      vim-devicons
+      coc-pyright
+      {
+        plugin = telescope-file-browser-nvim;
+        config = toLua "require(\"telescope\").load_extension \"file_browser\"";
+      }
+      {
+        plugin = telescope-project-nvim;
+        config = toLua "require\'telescope\'.load_extension(\'project\')";
+      }
+      {
+        plugin = telescope-github-nvim;
+        config = toLua "require(\'telescope\').load_extension(\'gh\')";
+      }
+      {
+        plugin = comment-nvim;
+        config = toLua "require(\"Comment\").setup()";
+      }
+      {
+        plugin = gruvbox-nvim;
+        config = "colorscheme gruvbox";
+      }
+    ];
+    extraPackages = with pkgs; [
+      wl-clipboard
+      gh
+      ripgrep
+      fd
+    ];
   };
 
   # programs.nixvim = {
@@ -354,7 +399,7 @@
     jabref
     poppler_utils
     manim
-    labplot
+    # labplot
     google-drive-ocamlfuse
     lynx
     ncgopher
